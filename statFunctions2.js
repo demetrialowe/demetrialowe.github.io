280b98c2805edd9553029ed370e8cda4bedbc452
demetrialowe <salowe@knox.edu>
2020-08-03 20:31:44 -0500

    Restarting Mostly
    I have no idea why this isn't working but fingers crossed the buttons still work

--- statFunctions2.js
+++ statFunctions2.js
@@ -26,36 +26,114 @@ function sumArray(array) {
 	return (array.reduce(reducer));
 }
 
+function writeToStatTable(rolls) {
+	var i = 0;
+	while(i<rolls.length) {
+		document.getElementById("roll"+i).innerHTML =rolls[i];
+		i++;
+	}
+}
+
+function clearStatTable() {
+	document.getElementById("STR").innerHTML = "";
+  	document.getElementById("DEX").innerHTML = "";
+  	document.getElementById("CON").innerHTML = "";
+  	document.getElementById("INTEL").innerHTML = "";
+  	document.getElementById("WIS").innerHTML = "";
+  	document.getElementById("CHA").innerHTML = "";
+}
+
 //this function rolls 3d6 six times, sums the results, 
 //and returns the array of stats in descending order
 function rollStat3d6() {
 	var rolls=[];
 	var i = 0;
+	clearStatTable();
 	while (i < 6) {
 		rolls.push(sumArray(rollXDice(6,3)));
 		i++;
 	}
 	rolls.sort(function(a,b){return b-a}); // b-a because descending order is desired
-	return rolls;
+	writeToStatTable(rolls);
 }
+
 
 //this function rolls 4d6, and drops the lowest result, sums the results, 
 //and returns the array of stats in descending order
 function rollStat4d6() {
 	var rolls = [];
+	var statArray = [];
 	var i =0;
+	clearStatTable();
 	while(i < 6) {
 		var fourd6 = rollXDice(6,4);
 		fourd6.sort(function(a,b){return b-a});
-		console.log(fourd6);
+		//console.log(fourd6);
 		fourd6.pop();
 		rolls.push(sumArray(fourd6));
 		i++;
 	}
-	console.log(rolls.sort(function(a,b){return b-a}));
+	rolls = rolls.sort(function(a,b){return b-a});
+	//console.log(rolls);
+	writeToStatTable(rolls);
 }
 
-/*thoughs
+function adjustStat() {
+	charStats[0]= document.getElementById("STR").innerHTML;
+	charStats[1]= document.getElementById("DEX").innerHTML;
+	charStats[2]= document.getElementById("CON").innerHTML;
+	charStats[3]= document.getElementById("INTEL").innerHTML;
+	charStats[4]= document.getElementById("WIS").innerHTML;
+	charStats[5]= document.getElementById("CHA").innerHTML;
+	console.log("My Stats: " + charStats);
+}
+
+function calculateMods(stat) {
+	if (document.getElementById(stat).innerHTML == "") {return ""};
+	var modifier = Math.floor((parseInt(document.getElementById(stat).innerHTML) - 10)/2);
+	return modifier;
+}
+
+function checkSkillRoot(skill) {
+	StrArray = ["athletics"];
+	DexArray = ["acrobatics","sleight of hand", "stealth"];
+	IntelArray = ["arcana","history","investigation","nature", "religion"];
+	WisArray = ["animal handling", "insight", "medicine","perception","survival"];
+	ChaArray = ["deception", "intimidation","performance","persuasion"];
+	if(StrArray.includes(skill)) return "str";
+	if(DexArray.includes(skill)) return "dex";
+	if(IntelArray.includes(skill)) return "intel";
+	if(WisArray.includes(skill)) return "wis";
+	if(ChaArray.includes(skill)) return "cha";
+	else return "invalid skill";
+}
+
+//Drag n drop section
+function allowDrop(ev) {
+	ev.preventDefault();
+}
+
+function drag(ev) {
+	ev.dataTransfer.setData("number", ev.target.id);
+}
+
+function drop(ev) {
+	var dataThere = ev.target.innerHTML;
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("number");
+	ev.target.innerHTML= document.getElementById(data).innerHTML;
+	document.getElementById(data).innerHTML = dataThere;
+	/*this lets the skill mods update while dragging and dropping*/
+	/*if I want to use drag and drop later I'm definitely going to have to rework this*/
+}
+
+function dropStat(ev) {
+	drop(ev);
+	adjustStat();
+}
+
+
+/*thoughts
 	1) map skills to indexes as listed below and save skills as an array
 	of modifiers mapped to the indexes. Proficiency can then be stored as potentially 
 	an array of 0 (for false/no proficiency selected) OR 1 (for true/proficient in this skill)
@@ -100,7 +178,7 @@ var raceAttr = {"hill dwarf":[0,0,2,0,1,0],
 "mountain dwarf":[2,0,2,0,0,0], 
 "high elf":[0,2,0,1,0,0], 
 "wood elf":[0,2,0,0,1,0], 
-"drow":[0,2,0,0,0,1]
+"drow":[0,2,0,0,0,1],
 "lightfoot halfling":[0,2,0,0,0,1],
 "stout halfling":[0,2,1,0,0,0],
 "human":[1,1,1,1,1,1],
